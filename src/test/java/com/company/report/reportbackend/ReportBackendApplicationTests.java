37e1701d16dd25e2fa91148be783e2ced27b43da
Tim <drtimde@hotmail.com>
2020-09-29 12:15:09 +0200

    Initial commit

---
+++ src/test/java/com/company/report/reportbackend/ReportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.report.reportbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
